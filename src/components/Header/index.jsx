4f766883c4acac3a9f3f8fda3833531866ff3428
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2024-10-07 11:50:19 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -4,7 +4,7 @@ import ScrollToBtn from '../Buttons/ScrollToBtn'
 
 function Header(props) {
   return (
-		<div className="header">
+	<div className="header">
 		<div className='header__nav'>
 			<img src="./assets/assis/Logotipo-Ótica-Assis-1.png" alt="Ótica Assis logo"></img>
 			<div className='header__nav--menu'>
@@ -13,17 +13,9 @@ function Header(props) {
 				<a href="https://api.whatsapp.com/send/?phone=553598652571&type=phone_number&app_absent=0"><button>Fale conosco</button></a>
 			</div>
 		</div>
-			<div className="header-banner" style={{...props.headerStyle, backgroundRepeat: "no-repeat"}}>
-			<div className='header__promo__text'>
-				<h1 className='promo__h1__mobile'><span>COMPROU,</span><br/> GANHOU!</h1>
-				<h1 className='promo__h1__pc'><span>COMPROU,</span><br/> GANHOU!</h1>
-			<div>
-				<h2>12X</h2>
-				<p>NO CARTÃO</p>
-			</div>
-			</div>
-					</div>
+		<div className="header-banner" style={{...props.headerStyle, backgroundRepeat: "no-repeat"}}>
 		</div>
+	</div>
   );
 }
 
