343034c8981e4a0986914357a38a7cf0e4f60700
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2025-02-05 10:34:51 -0300

    Update index.jsx

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,14 +3,16 @@ import WppButton from '../Buttons/wppBtn'
 import ScrollToBtn from '../Buttons/ScrollToBtn'
 
 function Header(props) {
-  return (
+  	const queryParameters = new URLSearchParams(window.location.search)
+	const phone = queryParameters.get("wpp") ? queryParameters.get("wpp") : "98652571"
+	return (
 	<div className="header">
 		<div className='header__nav'>
 			<img src="./assets/assis/Logotipo-Ótica-Assis-1.png" alt="Ótica Assis logo"></img>
 			<div className='header__nav--menu'>
 				<ScrollToBtn nome="Feminino" categoria="FEMININO--OCULOS_DE_GRAU"/>
 				<ScrollToBtn nome="Masculino" categoria="MASCULINO--OCULOS_DE_GRAU"/>
-				<a href="https://api.whatsapp.com/send/?phone=553598652571&type=phone_number&app_absent=0"><button>Fale conosco</button></a>
+				<a href={"https://api.whatsapp.com/send/?phone=5535" + phone + "&type=phone_number&app_absent=0"><button>Fale conosco</button></a>
 			</div>
 		</div>
 		<div className="header-banner" style={{...props.headerStyle, backgroundRepeat: "no-repeat"}}>
