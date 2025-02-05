189e3b509fe605f5dfb76922c70cb181b28c9c23
JeffVenancius <43701418+JeffVenancius@users.noreply.github.com>
2025-02-05 10:33:14 -0300

    Update index.jsx

--- src/components/Buttons/wppBtn/index.jsx
+++ src/components/Buttons/wppBtn/index.jsx
@@ -1,7 +1,9 @@
 function WppBtn(props) {
+	const queryParameters = new URLSearchParams(window.location.search)
+	const phone = queryParameters.get("wpp") ? queryParameters.get("wpp") : "98652571"
 	const wppIcon = <img src="./assets/assis/icone4.svg" style={{height:"70%", marginRight: "2%"}}/>
 	return (
-				<a href={"https://wa.me/553598652571?text=" + encodeURIComponent(props.mensagem)} target="_blank" rel="noreferrer" style={{textDecoration: "none", width:props.btnStyle.width, display:"flex", justifyContent:"center", alignItems: "center"}}>
+				<a href={"https://wa.me/5535" + phone + "?text=" + encodeURIComponent(props.mensagem)} target="_blank" rel="noreferrer" style={{textDecoration: "none", width:props.btnStyle.width, display:"flex", justifyContent:"center", alignItems: "center"}}>
 			<button style={{...props.btnStyle, width:"100%"}}>{props.icone && wppIcon}{props.texto}</button>
 		</a>
 	)
